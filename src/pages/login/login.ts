b77bdfcfea2b5aad174135a1166b1ceb7378bf7c
raju1979 <dhrajesh1@gmail.com>
2018-05-01 12:55:43 +0530

    login page ui done
    login page ui done

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-login',
@@ -8,11 +9,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  inputType:string = 'password';
+  inputToggleIcon = 'eye';
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _platform: Platform) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  getImageFromAssets(img):string{
+    let assetImg = '';
+
+    if(this._platform.is('mobile')){
+      assetImg = `assets/imgs/${img}`
+    }else{
+      assetImg = `../../assets/imgs/${img}`;
+    }
+
+    // console.log(assetImg)
+    return assetImg;
+  };//
+
+  getLoadingImg() {
+    if (this._platform.is('core')) {
+      return "../assets/imgs/loading.svg";
+    } else if (this._platform.is('android')) {
+      return "assets/imgs/loading.svg";
+    } else {
+      return "assets/imgs/loading.svg";
+    }
+  };//
+
+  toggleInputType() {
+    if(this.inputType === 'text') {
+      this.inputType = 'password';
+      this.inputToggleIcon = 'eye';
+    } else {
+      this.inputType = 'text';
+      this.inputToggleIcon = 'eye-off';
+    }
+  }
+
+  onLoginSubmit(f) {
+    if(f.valid) {
+      console.log(f);
+    }
+  }
+
+  forgotPassword() {
+    window.open("https://google.com", "_system");
   }
 
 }
